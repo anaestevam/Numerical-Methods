911b78f7b682dfeb5b78e3bb04ab7a73f625698f
Ana Luisa <53523588+anaestevam@users.noreply.github.com>
2022-05-16 20:08:00 -0300

    Add files via upload

---
+++ src/newthonrapshon.py
@@ -0,0 +1,15 @@
+def f(x):
+    return(x**2-2*x-3)
+
+def fi(x):
+    return(2*x-2)
+
+def hata(x1,x2):
+    return((x1-x2)/x1)
+
+x1 = int(input("insira um valor inicial: "))
+
+for i in range(5):
+    x2 = x1 - f(x1)/fi(x1)
+    print(x1, x2, hata(x2,x1))
+    x1 = x2
