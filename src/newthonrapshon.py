f49686c0b956eb3365c7b3fb49c33cfa3fb0a34f
Ana Luisa <53523588+anaestevam@users.noreply.github.com>
2022-05-23 21:53:06 -0300

    Update newthonrapshon.py

--- src/newthonrapshon.py
+++ src/newthonrapshon.py
@@ -1,8 +1,8 @@
 def f(x):
-    return(x**2-2*x-3)
+    return(x**4 - 2.36343*(x**3) - 18.1163*(x**2) + 20.7595*x + 58.8273)
 
 def fi(x):
-    return(2*x-2)
+    return(4*(x**4)- 7.09029*(x**2)-36.2326*x + 20.7595)
 
 def hata(x1,x2):
     return((x1-x2)/x1)
@@ -13,3 +13,4 @@ for i in range(5):
     x2 = x1 - f(x1)/fi(x1)
     print(x1, x2, hata(x2,x1))
     x1 = x2
+
