06b8a8f15174e85402b304f760b19d7716c2076c
Ana Luisa <53523588+anaestevam@users.noreply.github.com>
2022-07-19 11:01:14 -0300

    Add files via upload

--- src/3 unidade/regressao.py
+++ src/3 unidade/regressao.py
@@ -1,19 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-def f(x):
-    b0+b1*x
-b1 = 4 #coeficientes
-ptsx = np.linspace(-5,5,100) #varias abcissas
+#coeficientes
+b0 = 2
+b1 = 4 
+ptsx = np.linspace(-5,5,100) #varias abscissas
 m = len(ptsx) #tamanho da lista
-ptsy = ptsx*b1+ np.random.normal(0, 2, m) #lista com ordenadas somando 1 a 1
+ptsy = ptsx*b1+ np.random.normal(b0, 1.5, m) #lista com ordenadas somando 1 a 1
 
 numerador = sum(ptsx*ptsy)
 denominador = sum(ptsx*ptsx)
-b1_calculado = numerador/denominador
-print(b1_calculado)
+#calculando a media das listas
+x_mean = np.mean(ptsx)
+y_mean = np.mean(ptsy)
 
-plt.scatter(ptsx, ptsy)
-plt.plot(ptsx, b1_calculado*ptsx)
+a1_calculado = numerador/denominador
+a0_calculado = y_mean - a1_calculado*x_mean
+
+print('valor de a1 = ',a1_calculado)
+print('valor de a0 = ',a0_calculado)
+
+#Reta de regressão
+r = a1_calculado * ptsx + a0_calculado
+
+plt.scatter(ptsx, ptsy, color = 'red')
+plt.plot(ptsx, r)
 plt.show()
-
